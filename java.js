69b7162fb91d4b22b9d268b0c1d2694801b3fcc1
Malena96 <136396033+Malena96@users.noreply.github.com>
2023-06-12 20:59:09 -0300

    Initial commit

---
+++ java.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    alert("hola!")
+    
+}
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
